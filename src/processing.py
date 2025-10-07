2b5804460a644915608b98981168e3a4fbc0b196
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2025-10-07 22:53:33 +0300

    Исправлены небольшие ошибки вызова black и flake8

--- src/processing.py
+++ src/processing.py
@@ -18,16 +18,3 @@ def sort_by_date(transactions: List[Dict[str, Any]], reverse: bool = True) -> Li
 
     sorted_transactions_by_date = sorted(transactions, key=lambda x: x["date"], reverse=reverse)
     return sorted_transactions_by_date
-
-
-data = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-print(filter_by_state(data, state="CANCELED"))
-print(filter_by_state(data))
-print(sort_by_date(data))
-print(sort_by_date(data, reverse=False))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
 
+
 # Тесты для get_mask_card_number
 @pytest.mark.parametrize(
     "card_number, expected_mask",
@@ -14,10 +15,10 @@ from src.masks import get_mask_account, get_mask_card_number
         (" 7000 7922 8960 6361 ", "7000 79** **** 6361"),
     ],
 )
-
 def test_get_mask_card_number_valid(card_number: str, expected_mask: str) -> None:
     result = get_mask_card_number(card_number)
     assert result == expected_mask
+
 
 # Тесты для get_mask_account
 @pytest.mark.parametrize(

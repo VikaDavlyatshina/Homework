cec9a1ff85ba9676ddb94715b19a70e5ec79a195
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2025-10-13 14:40:13 +0300

    Добавлен декоратор @wraps

--- src/decorators.py
+++ src/decorators.py
@@ -1,23 +1,33 @@
+from functools import wraps
+
 def log(filename=None):
-        """Декоратор, который пишет, успешно ли выполнена функция."""
+        """Декоратор, который автоматически регистрирует детали выполнения функции."""
 
         def decorator(func):
+            @wraps(func)
             def wrapper(*args, **kwargs):
                 try:
+                    # При успешном выполнении функции, генерируется сообщение об успехе
                     result = func(*args, **kwargs)
                     message = f"{func.__name__} успешно выполнена."
+
                     if filename:
+                        # Если указан файл, записываем результат работы функции в указанный файл
                         with open(filename, 'a', encoding='utf8') as f:
                             f.write(message + '\n')
                     else:
+                        # Если файл не указан, выводим сообщение в консоль
                         print(message)
                     return result
                 except Exception as e:
+                    # Генерируется сообщение об ошибке
                     error_msg = f"{func.__name__} не выполнена. Ошибка: {type(e).__name__}."
                     if filename:
+                        # Если файл указан, информация об ошибке записывается в файл
                         with open(filename, 'a', encoding='utf8') as f:
                             f.write(error_msg + '\n')
                     else:
+                        # Иначе выводится в консоль
                         print(error_msg)
 
             return wrapper

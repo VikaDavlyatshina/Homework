4a466aab4c59dcfe0b3a81c2b20129a89cb25548
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2025-10-13 19:22:32 +0300

    Создан модуль external_api.py. В нём записана функция для конвертации транзакций в рубли

--- src/external_api.py
+++ src/external_api.py
@@ -5,14 +5,61 @@ import requests
 # Загрузка переменных из .env файла
 load_dotenv()
 
-#Получение токена API
-APIKEY = os.getenv('API_KEY')
-BASE_URL = "https://api.apilayer.com/exchangerates_data/convert"
+APIKEY = "MjvYNFpGpf8CKm40Ck1aP06zv9Vawrci"
+# APIKEY = os.getenv('API_KEY') #Получение токена API
+url= "https://api.apilayer.com/exchangerates_data/convert"
 
-def get_sum_transactions(transaction: dict) -> float:
+def convert_transactions_to_rub(transaction: dict) :
     """Функция принимает на вход транзакцию и возвращает сумму транзакций (amount) в рублях"""
 
-    pass
+    if not APIKEY:
+        print("API_KEY не найден в переменных окружения")
+        return None
+    try:
+        # Извлекаем сумму и код валюты
+        amount = float(transaction["operationAmount"]["amount"])
+        currency_code = transaction["operationAmount"]['currency']['code']
+
+        if currency_code == 'RUB':
+            return amount
+
+        if currency_code in ['USD', 'EUR']:
+            headers = {'apikey': APIKEY}
+            params = {
+                'from': currency_code,
+                'to': 'RUB',
+                'amount': amount
+            }
+
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+
+            data = response.json()
+            if 'result' in data:
+                return  float(data['result'])
+
+            else:
+                print("Ошибка API: отсутствует 'result' в ответе.")
+                return None
+        else:
+             print(f"Неподдерживаемая валюта: {currency_code}")
+             return None
+
+    except KeyError as e:
+        print(f"Отсутствует обязательное поле в транзакции: {e}")
+        return None
+    except ValueError as e:
+        print(f"Ошибка в преобразовании данных: {e}")
+        return None
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка обращения к API: {e}")
+        return None
 
 
-    return amount
+
+
+
+
+
+
+
